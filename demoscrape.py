2b6cd31cf46749cdd7942707a68b09ed02489b7a
Terry Brown <terrynbrown@gmail.com>
2019-05-05 18:32:58 -0400

    works, switching examples...

--- demoscrape.py
+++ demoscrape.py
@@ -5,7 +5,11 @@ from lxml import etree
 
 CACHE_FILE = "scrape.cache.json"
 
-URL = "https://www.epa.gov/ground-water-and-drinking-water/national-primary-drinking-water-regulations"
+START_URL = (
+    "https://www.epa.gov/ground-water-and-drinking-water/"
+    "national-primary-drinking-water-regulations"
+)
+
 
 def get_url(data, url):
     if url not in data['url']:
@@ -16,25 +20,47 @@ def get_url(data, url):
 
 
 def do_stuff(data):
-    html = get_url(data, URL)
-
-    sub_urls = get_subs_recursive(data, html)
-
-def get_subs_recursive(html):
-
+    # get html str for START_URL
+    html = get_url(data, START_URL)
+    # get the lenient HTML parser, not the default strict XML parser
     parser = etree.HTMLParser()
-    tree = etree.parse(html, parser)
-    return tree
+    # parse the text into a tree of Element nodes
+    tree = etree.XML(html, parser)
+    # get the list of target urls using recursion
+    sub_urls = recurse_tree(tree)
+    print(sub_urls)
+    # get the same list with XPath
+    sub_urls = tree.xpath("//div[contains(@class, 'node-page')]/ul//a/@href")
+    print(sub_urls)
+
+    for sub_url in sub_urls:  # actually links within the same page
+        pass
+
+
+def recurse_tree(node):
+    if node.tag == 'div' and 'node-page' in node.get('class', ''):
+        node = [i for i in node if i.tag == 'ul'][0]
+        lis = [i for i in node]
+        hrefs = [i[0].get('href') for i in lis]
+        return hrefs
+    else:
+        for child in node:
+            ans = recurse_tree(child)
+            if ans:
+                return ans
+    return None
+
 
 def main():
     if not os.path.exists(CACHE_FILE):
-        data = {'url':{}}
+        data = {'url': {}}
     else:
         data = json.load(open(CACHE_FILE))
     try:
         do_stuff(data)
     finally:
         json.dump(data, open(CACHE_FILE, 'w'))
+
 
 if __name__ == "__main__":
     main()
